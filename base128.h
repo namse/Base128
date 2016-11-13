7ad807b74163e10703127e3ab47bccccfb9cae17
Nam Se Hyun <skatpgusskat@users.noreply.github.com>
2016-11-13 22:13:02 +0900

    Create base128.h

---
+++ base128.h
@@ -0,0 +1,40 @@
+#pragma once
+
+bool ParseFromByte128(
+		uint32_t &value,
+		char* const buffer,
+		const std::size_t max_bytes,
+		std::size_t &read) {
+	uint32_t ret = 0;
+	for (std::size_t i = 0; i < max_bytes; i++) {
+		ret += (buffer[i] & 0b01111111) << (7 * i);
+		if (!(buffer[i] & 0b10000000)) {
+			value = ret;
+			read = i + 1;
+			return true;
+		}
+	}
+	return false;
+}
+
+bool SerializeToByte128(
+		const uint32_t target,
+		char* const buffer,
+		const std::size_t max_bytes,
+		std::size_t &written) {
+	uint32_t copied = target;
+	std::size_t index = 0;
+	while (copied) {
+		if (index > 0) {
+			buffer[index - 1] |= 0b10000000;
+		}
+		buffer[index] = copied & 0b01111111;
+		copied >>= 7;
+		++index;
+		if (index > max_bytes) {
+			return false;
+		}
+	}
+	written = index;
+	return true;
+}
